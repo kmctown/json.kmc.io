07535f5c64dda6a922ae2f7647d90bd28df13d37
Kris Ciccarello <kciccarello@gmail.com>
2015-10-19 20:08:50 -0400

    Added gulp-connect server and livereload.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import uglify from "gulp-uglify"
 import htmlmin from "gulp-htmlmin"
 import rename from "gulp-rename"
 import csso from "gulp-csso"
+import connect from "gulp-connect"
 import runSequence from "run-sequence"
 
 gulp.task("clean", () => {
@@ -15,6 +16,7 @@ gulp.task("js", () => {
     .pipe(uglify())
     .pipe(rename("/js/main.min.js"))
     .pipe(gulp.dest("dist"))
+    .pipe(connect.reload())
 })
 
 gulp.task("vendorjs", () => {
@@ -33,6 +35,7 @@ gulp.task("html", () => {
   return gulp.src("./index.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
 })
 
 gulp.task("css", () => {
@@ -40,12 +43,26 @@ gulp.task("css", () => {
     .pipe(csso())
     .pipe(rename("/css/main.min.css"))
     .pipe(gulp.dest("dist"))
+    .pipe(connect.reload())
 })
 
 gulp.task("vendorcss", () => {
   return gulp.src("./css/*.min.css")
     .pipe(rename("/css/vendor.min.css"))
     .pipe(gulp.dest("dist"))
+})
+
+gulp.task("serve", ["default", "watch"], () => {
+  connect.server({
+    root: "dist",
+    livereload: true
+  })
+})
+
+gulp.task("watch", () => {
+  gulp.watch(["./*.html"], ["html"]);
+  gulp.watch(["./js/*.js"], ["js"]);
+  gulp.watch(["./css/*.css"], ["css"]);
 })
 
 gulp.task("default", ["clean"], (cb) => {
